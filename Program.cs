582f302546259fd20b9cab5f93abfdc500e1f2f5
Luis Perez <Luis.Perez@checkmarx.com>
2022-09-27 10:50:56 +0100

    second commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Net.NetworkInformation;
+using Elasticsearch.Net;
+using Nest;
+
+Console.WriteLine("Hello, World!");
+
+
+var test = new ElasticClient();
+
+var sd = new ElasticLowLevelClient();
+
+var df = new ElasticLowLevelClient();
+
+var sdf = new ElasticsearchUrlFormatter(null);
+
+var sf = new UserAgentProcessor();
+
+new ElasticsearchClientException("");
+
+
